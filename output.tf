83fb7055d83afb5ebf47e3c933214ff745c33dda
BaniprasadMangaraj <baniprasadmangaraj2@gmail.com>
2025-04-03 16:50:43 +0530

    Added output with name and ip

--- output.tf
+++ output.tf
@@ -1,4 +1,7 @@
 output "ec2_public_ips" {
-  description = "List of public IP addresses of the EC2 instances"
-  value       = [for instance in aws_instance.my_instance : instance.public_ip]
+  description = "List of public IP addresses and names of the EC2 instances"
+  value = [for instance in aws_instance.my_instance : {
+    name      = instance.tags.Name
+    public_ip = instance.public_ip
+  }]
 }
